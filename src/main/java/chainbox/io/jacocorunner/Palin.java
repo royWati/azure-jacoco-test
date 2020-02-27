91655a4f8ce386bb0fae53726bdd29ae4ace1cc9
Roy Munialo Wati <munialo.roy@ekenya.co.ke>
2020-02-27 18:14:03 +0300

    changes in Palin

--- src/main/java/chainbox/io/jacocorunner/Palin.java
+++ src/main/java/chainbox/io/jacocorunner/Palin.java
@@ -16,6 +16,7 @@ public class Palin {
         if (inputString.length() == 0) {
             return true;
         } else {
+
             char firstChar = inputString.charAt(0);
             char lastChar = inputString.charAt(inputString.length() - 1);
             String mid = inputString.substring(1, inputString.length() - 1);
